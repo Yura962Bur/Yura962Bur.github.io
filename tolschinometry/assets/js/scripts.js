f1c59732656ee6c887cdce87fb6f153c81fdbf0e
Yura962Bur <53567802+Yura962Bur@users.noreply.github.com>
2020-11-16 14:31:40 +0300

    Add files via upload

---
+++ tolschinometry/assets/js/scripts.js
@@ -0,0 +1,13 @@
+//$(document).ready(function() {
+  var mySwiper = new Swiper('.swiper-container', {
+    speed: 400,
+    spaceBetween: 10,
+    loop: true,
+    slidesPerView: 1,
+    slidesPerGroup: 1,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      }
+  })
+//});
