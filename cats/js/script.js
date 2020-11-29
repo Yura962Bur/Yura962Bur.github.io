09ca8296e42850fa4849a190f02c9f20ce0bc7ab
Yuriy <Yura962Bur@mail.ru>
2020-11-29 19:42:21 +0300

    js for cats part 1

--- cats/js/script.js
+++ cats/js/script.js
@@ -1,23 +1,25 @@
 
 //Лайк
-// let like=document.querySelector('.like');
+let like = document.querySelectorAll('.like');
+for(let i = 0; i < like.length; i++){
+    like[i].onclick=function() {
+        like[i].classList.toggle('like-clicked');
+    }
+}
 
-// like.onclick=function() {
-//     like.classList.toggle('like-clicked');
-// }
 
-
-// //Кнопка "Купить"
-// let buyBtn=document.querySelector('.buy');
-
-// buyBtn.onclick=function() {  
-//     buyBtn.classList.toggle('buy-clicked');
-//     if(buyBtn.textContent == 'Купить'){
-//         buyBtn.textContent = 'Продан';
-//     }else{
-//         buyBtn.textContent = 'Купить';
-//     }
-// }
+//Кнопка "Купить"
+let buyBtn = document.querySelectorAll('.buy');
+for(let i = 0; i < buyBtn.length; i++){
+    buyBtn[i].onclick=function() {  
+        buyBtn[i].classList.toggle('buy-clicked');
+        if(buyBtn[i].textContent == 'Купить'){
+            buyBtn[i].textContent = 'Продан';
+        }else{
+            buyBtn[i].textContent = 'Купить';
+        }
+    }
+}
 
 //Прокрутка
 let header = document.getElementById('header');
